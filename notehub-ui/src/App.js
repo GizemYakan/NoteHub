995036c55ecd3029bb48b41cf814ea43f60c3b97
GIZEM YAKAN <gizem.yakan9@gmail.com>
2021-05-28 10:33:41 +0300

    reactjs files

---
+++ notehub-ui/src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div>
+      <h1>Merhaba Dünya</h1>
+      <Button variant="primary">Benim butonum</Button>
+      <Button variant="danger">Benim butonum</Button>
+    </div>
+  );
+}
+
+export default App;
