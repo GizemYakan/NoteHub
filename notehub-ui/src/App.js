297e1c0655581a2d1fc39b73e1f399eb42b3c461
GIZEM YAKAN <gizem.yakan9@gmail.com>
2021-05-28 11:29:31 +0300

    register

--- notehub-ui/src/App.js
+++ notehub-ui/src/App.js
@@ -1,14 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Button from 'react-bootstrap/Button';
+import Register from './Register'
 
 function App() {
   return (
-    <div>
-      <h1>Merhaba Dünya</h1>
-      <Button variant="primary">Benim butonum</Button>
-      <Button variant="danger">Benim butonum</Button>
-    </div>
+   <Register />
   );
 }
 
